8663fb61216db25e329f9fe6c8114373085b9043
Yash Jain <113816624+YashJain999@users.noreply.github.com>
2024-06-17 00:05:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GymLanding from './GymLanding';
+
+function App() {
+  return (
+    <div className="">
+      <GymLanding />
+    </div>
+  );
+}
+
+export default App;
