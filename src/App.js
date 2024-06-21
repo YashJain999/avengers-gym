3ad12048ca62b3a6d7eb894037e735b052be7f1b
UrviJoshi24 <urvigithub334@gmail.com>
2024-06-21 09:20:06 +0530

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import GymLanding from './GymLanding';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="">
+      
       <GymLanding />
+      <Footer />
     </div>
   );
 }
